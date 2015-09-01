5fa245eb2a2c3c038fa417682e3fabebeff668e1
Chandru Ds <chandru.ds@hpe.com>
2015-09-01 03:52:33 -0700

    autoprovision script code clean up
    Change-Id: I1db80cbfc66542165215db5d4cbe98f4cdfcda25
    Signed-off-by: Chandru Ds <chandru.ds@hpe.com>

--- autoprovision.py
+++ autoprovision.py
@@ -16,116 +16,91 @@
 
 import os
 import sys
-import argparse
-import json
 import urllib2
 import httplib
 import cookielib
 from time import sleep
 
 import ovs.dirs
+import ovs.db.idl
 from ovs.db import error
 from ovs.db import types
-import ovs.util
-import ovs.db.idl
 
 # ovs definitions
 idl = None
 # HALON_TODO: Need to pull this from the build env
-def_db = 'unix:/var/run/openvswitch/db.sock'
+DEF_DB = 'unix:/var/run/openvswitch/db.sock'
+CFGDB_SCHEMA = '/usr/share/openvswitch/configdb.ovsschema'
+OVS_SCHEMA = '/usr/share/openvswitch/vswitch.ovsschema'
 
 # Configuration file definitions
 saved_config = None
-# HALON_TODO: Need to pull these three from the build env
-cfgdb_schema = '/usr/share/openvswitch/configdb.ovsschema'
-ovs_schema = '/usr/share/openvswitch/vswitch.ovsschema'
+
 type_startup_config = "startup"
 max_miliseconds_to_wait_for_config_data = 30
 
 OPEN_VSWITCH_TABLE = "Open_vSwitch"
 HALON_TRUE = "True"
-HALON_FALSE = "False"
 PERFORMED = "performed"
 URL = "url"
 AUTOPROVISION_FILE = '/var/tmp/autoprovision'
 
-def is_system_configured(data):
-    '''
-    Check the configuration initialization completed Open_vSwitch:cur_cfg
-    configuration completed: return True
-    else: return False
-    '''
+#------------------ wait_for_config_complete() ----------------
+def wait_for_config_complete(idl):
 
-    for ovs_rec in data[OPEN_VSWITCH_TABLE].rows.itervalues():
-        if ovs_rec.cur_cfg:
-            if ovs_rec.cur_cfg > 0:
-                return True
-            else:
-                return False
+    system_is_configured = 0
+    while system_is_configured == 0:
+        for ovs_rec in idl.tables[OPEN_VSWITCH_TABLE].rows.itervalues():
+            if ovs_rec.cur_cfg is not None and ovs_rec.cur_cfg != 0:
+               system_is_configured = ovs_rec.cur_cfg
+               break
 
-    return False
-
-#------------------ get_config() ----------------
-def get_config(idl_cfg):
-    '''
-    Walk through the rows in the config table (if any)
-    looking for a row with type == startup.
-
-    If found, set global variable saved_config to the content
-    of the "config" field in that row.
-    '''
-
-    global saved_config
-
-    #Note: You can't tell the difference between the config table not
-    #      existing (that is the configdb is not there) or just that there
-
-    #      are no rows in the config table.
-    tbl_found = False
-    for ovs_rec in idl_cfg.tables["config"].rows.itervalues():
-        tbl_found = True
-        if ovs_rec.type:
-            if ovs_rec.type == type_startup_config:
-                if ovs_rec.config:
-                    saved_config = ovs_rec.config
-                else:
-                    print("startup config row does not have config column")
-                return
-
-    if not tbl_found:
-        print("No rows found in the config table")
+        poller = ovs.poller.Poller()
+        idl.run()
+        idl.wait(poller)
+        poller.block()
 
 #------------------ check_for_startup_config() ----------------
 def check_for_startup_config(remote):
     '''
     Connect to the db server and specify the configdb database.
-    Look for an entry with type=startup
-    If exists, read the configuration.
+    Walk through the rows in the config table (if any)
+    looking for a row with type == startup.
     '''
 
-    global saved_config
-
-    saved_config = None
-
-    schema_helper_cfg = ovs.db.idl.SchemaHelper(location=cfgdb_schema)
+    schema_helper_cfg = ovs.db.idl.SchemaHelper(location=CFGDB_SCHEMA)
     schema_helper_cfg.register_table("config")
 
     idl_cfg = ovs.db.idl.Idl(remote, schema_helper_cfg)
 
-    '''
-    The wait time is 30 * 0.1 = 3 seconds
-    '''
-    cnt = max_miliseconds_to_wait_for_config_data
-    while not idl_cfg.run() and cnt > 0:
-        cnt -= 1
-        sleep(.1)
+    seqno = idl_cfg.change_seqno  # Sequence number when we last processed the db.
 
-    get_config(idl_cfg)
+    # Wait until the ovsdb sync up.
+    while (seqno == idl_cfg.change_seqno):
+        idl_cfg.run()
+        poller = ovs.poller.Poller()
+        idl_cfg.wait(poller)
+        poller.block()
+
+    tbl_found = False
+
+    for ovs_rec in idl_cfg.tables["config"].rows.itervalues():
+        if ovs_rec.type:
+            if ovs_rec.type == type_startup_config:
+                if ovs_rec.config:
+					tbl_found = True
+                else:
+                    print("startup config row does not have config column")
+                break
+
+    if not tbl_found:
+        print("No startup config rows found in the config table")
 
     idl_cfg.close()
 
-    return
+    return tbl_found
 
+#------------------ fetch_autoprovision_script() ----------------
 def fetch_autoprovision_script(url):
     ret = False
     try :
@@ -155,29 +130,18 @@ def fetch_autoprovision_script(url):
     if os.path.exists(AUTOPROVISION_FILE):
         os.remove(AUTOPROVISION_FILE)
 
-    if ("HALON-AUTOPROVISIONING" in data):
+    if ("OPS-PROVISIONING" in data):
         FILE = open(AUTOPROVISION_FILE, "wb")
         FILE.write(data)
         FILE.close()
         ret = True
     else :
-        print("Error, downloaded autoprovision script doesn't contain HALON-AUTOPROVISIONING string in comment")
+        print("Error, downloaded autoprovision script doesn't contain OPS-PROVISIONING string in comment")
         ret = False
 
     return ret
 
-def is_autoprovision_performed():
-    global idl
-
-    ret = False
-    for ovs_rec in idl.tables[OPEN_VSWITCH_TABLE].rows.itervalues():
-        if ovs_rec.auto_provisioning_status:
-            for key, value in ovs_rec.auto_provisioning_status.items():
-                if key == PERFORMED and value == HALON_TRUE:
-                    ret = True
-
-    return ret
-
+#------------------ update_autoprovision_status() ----------------
 def update_autoprovision_status(performed_value, url):
     global idl
 
@@ -208,41 +172,32 @@ def main():
         return
 
     # Locate default config if it exists
-    schema_helper = ovs.db.idl.SchemaHelper(location=ovs_schema)
-    schema_helper.register_columns(OPEN_VSWITCH_TABLE, \
-                ["cur_cfg", "auto_provisioning_status"])
+    schema_helper = ovs.db.idl.SchemaHelper(location=OVS_SCHEMA)
+    schema_helper.register_columns(OPEN_VSWITCH_TABLE, ["cur_cfg"])
+    schema_helper.register_columns(OPEN_VSWITCH_TABLE, ["auto_provisioning_status"])
 
-    idl = ovs.db.idl.Idl(def_db, schema_helper)
+    idl = ovs.db.idl.Idl(DEF_DB, schema_helper)
 
     seqno = idl.change_seqno    # Sequence number when we last processed the db
 
-    '''
-    The wait time is 30 * 0.1 = 3 seconds
-    '''
-    cnt = max_miliseconds_to_wait_for_config_data
-    while not idl.run() and cnt > 0:
-        cnt -= 1
-        sleep(.1)
+    # Wait until the ovsdb sync up.
+    while (seqno == idl.change_seqno):
+        idl.run()
+        poller = ovs.poller.Poller()
+        idl.wait(poller)
+        poller.block()
 
+    wait_for_config_complete(idl)
 
-    #Wait till system is configured
-    while not is_system_configured(idl.tables):
-        sleep(1)
-
+	# HALON_TODO : Need to find a better logic.
     if os.path.exists('/etc/autoprovision'):
         print("Autoprovisioning already completed")
         update_autoprovision_status(HALON_TRUE, argv[1])
         idl.close()
         return
 
-    check_for_startup_config(def_db)
-
-    if(saved_config != None):
+    if(check_for_startup_config(DEF_DB) == True):
         print("startup config present, skipping autoprovision")
-        idl.close()
-        return
-
-    if(is_autoprovision_performed() == True):
         idl.close()
         return
 
@@ -262,7 +217,7 @@ def main():
             os.system('touch /etc/autoprovision')
             print("Autoprovision status: performed = %s URL =  %s" % (HALON_TRUE, argv[1]))
         else:
-            print("Error, autoprovision script returned error %d" % ret)
+            print("Error, executing autoprovision script returned error %d" % ret)
 
     idl.close()
 
