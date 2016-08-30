e7d69abc50d451b3e77237c6e3108fba309dae9f
Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>
2016-08-30 14:10:06 -0700

    Minor changes to AAA daemon
    Change-Id: I67bf39f2ea731cf139447e29be006a8a5a65c904
    Signed-off-by: Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>

--- include/aaa_vty.h
+++ include/aaa_vty.h
@@ -62,7 +62,7 @@ typedef struct aaa_server_group_prio_params_s {
 #define SYSTEM_AAA_LOCAL                "local"
 #define SYSTEM_AAA_RADIUS               "radius"
 #define SYSTEM_AAA_TACACS               "tacacs"
-#define SYSTEM_AAA_TACACS_PLUS          "tacacs+"
+#define SYSTEM_AAA_TACACS_PLUS          "tacacs"
 #define SYSTEM_AAA_FALLBACK             "fallback"
 #define SYSTEM_AAA_RADIUS_LOCAL         "local"
 #define SYSTEM_AAA_RADIUS_AUTH          "radius_auth"
